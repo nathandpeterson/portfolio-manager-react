170ae8788db587eb9141c49d00f52c1a166cfc42
Nathan Daniel Peterson <natperson@gmail.com>
2018-11-04 20:22:04 -0800

    Handle when user sends an email through the form.

--- src/components/Forms/EmailForm.jsx
+++ src/components/Forms/EmailForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Row, Col, Input, Button, Icon } from 'react-materialize'
 import Nav from '../Nav'
 import { sendEmail } from '../../actions'
@@ -6,62 +6,110 @@ import { connect } from 'react-redux'
 
 class EmailForm extends Component {
 
-  constructor(){
+  constructor() {
     super()
 
     this.state = {
       emailContent: '',
       email: '',
-      name: ''
+      name: '',
+      message: '',
+      error: false,
+      messageSent: false,
     }
   }
 
-  render(){
+  componentDidUpdate(prevProps) {
+    if (this.props.emailResponse.message && !prevProps.emailResponse.message) {
+      this.handleEmailSubmission()
+    }
+  }
+
+  handleEmailSubmission() {
+    if (this.props.emailResponse.message === 'success') {
+      this.setState({ message: 'Your email was sent. Thank you.', messageSent: true })
+    } else {
+      this.setState({ error: true })
+      setTimeout(() => {
+        this.setState({ error: false })
+      }, 2500)
+    }
+  }
+
+  renderContactForm = () => (
+    <Fragment>
+      <Row>
+        <Col s={3} />
+        <Input
+          s={6}
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+          label={'Your Name'} />
+      </Row>
+      <Row>
+        <Col s={3} />
+        <Input
+          s={6}
+          label="Your Email"
+          type='email'
+          validate
+          value={this.state.email}
+          onChange={(e) => this.setState({ email: e.target.value })}
+        />
+      </Row>
+      <Row>
+        <Col s={3} />
+        <Input label={'Message'}
+          s={6}
+          onChange={(e) => this.setState({ emailContent: e.target.value })}
+          placeholder='Write your message here...'
+          type='textarea' />
+      </Row>
+    </Fragment>
+  )
+
+  renderError = () => (
+    <div className='flex-center'>
+      <h5 style={{ fontWeight: 400 }}>
+        There was an error sending your message. Please try again.
+        <Button className='#03a9f4 light-blue'
+          onClick={() => this.setState({ error: false, messageSent: false })}
+        >TRY AGAIN
+        </Button>
+      </h5>
+    </div>
+  )
+
+  render() {
+    const { messageSent, message } = this.state
     return (
       <div>
         <Nav />
-        <Row>
-          <Col s={3}/>
-          <Input 
-                  s={6}
-                  value={this.state.name}
-                  onChange={(e) => this.setState({name: e.target.value})}
-                  label={'Your Name'}/>
-        </Row>
-        <Row>
-          <Col s={3}/>
-          <Input 
-                 s={6} 
-                 label="Your Email" 
-                 type='email' 
-                 validate 
-                 value={this.state.email}
-                 onChange={(e) => this.setState({ email: e.target.value})}
-                 />
-        </Row>
-        <Row>
-          <Col s={3}/>
-          <Input  label={'Message'} 
-                  s={6}
-                  onChange={(e) => this.setState({emailContent: e.target.value})}
-                  placeholder='Write your message here...'  
-                  type='textarea' />
-        </Row>
+        {!messageSent && this.renderContactForm()}
+        {message && <div className='header flex-center'>
+          <h5 style={{ fontWeight: 400 }}>{this.state.message}</h5>
+        </div>}
+        {this.state.error && this.renderError()}
         <div className='flex-center'>
-          <Button onClick={() => this.props.sendEmail(this.state)}>
-            <Icon>
+          {!messageSent && <Button className='#03a9f4 light-blue'
+            onClick={() => {
+              const { emailContent, email, name } = this.state
+              this.props.sendEmail({ emailContent, email, name, messageSent: true, message: '' })
+            }
+            }> <Icon>
               email
-            </Icon>
+                </Icon>
           </Button>
+          }
         </div>
       </div>
-      
+
     )
   }
 }
 
 const mapStateToProps = state => (
-  {emailRespose: state.emailResponse}
+  { emailResponse: state.emailResponse }
 )
 
 const mapDispatchToProps = dispatch => (
